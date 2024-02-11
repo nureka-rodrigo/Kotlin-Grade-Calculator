e9bc6fdaaac2c15b0f08c8942d48fd3762dd043f
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-02-11 10:00:31 +0530

    Added comments

--- app/src/main/java/com/example/kotlin_grade_calculator/SplashActivity.kt
+++ app/src/main/java/com/example/kotlin_grade_calculator/SplashActivity.kt
@@ -1,19 +1,27 @@
 package com.example.kotlin_grade_calculator
 
+// Import necessary Android and Kotlin libraries
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
+// Define the SplashActivity class, which extends AppCompatActivity
 class SplashActivity : AppCompatActivity() {
+    // Override the onCreate method from AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Call the superclass's onCreate method with the savedInstanceState
         super.onCreate(savedInstanceState)
+        // Set the content view to the splash activity layout
         setContentView(R.layout.activity_splash)
 
+        // Create a new Handler to post a delayed Runnable to the main thread
         Handler(Looper.getMainLooper()).postDelayed({
+            // Inside the Runnable, start the MainActivity
             startActivity(Intent(this, MainActivity::class.java))
+            // Finish the SplashActivity so the user can't go back to it
             finish()
-        }, 3000) // 3000 is the delayed time in milliseconds.
+        }, 3000) // Delay the Runnable by 3000 milliseconds (3 seconds)
     }
 }
