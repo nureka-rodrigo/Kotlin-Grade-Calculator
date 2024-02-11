65fff53c6c5a215d711ece5f6e16c3a1563d037a
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-02-11 09:55:47 +0530

    Added splash screen

---
+++ app/src/main/java/com/example/kotlin_grade_calculator/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin_grade_calculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
